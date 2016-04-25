4c23cd539aa36ffed3e722f65cde62a194714f30
Kornel Kielczewski <kornel.kielczewski@allegrogroup.com>
2016-04-25 11:16:37 +0200

    Test for run90 and run50

---
+++ run90.sh
@@ -0,0 +1,10 @@
+python TEMT.py \
+     -p example_data/b_proportion_90/type_a.sam \
+     -m example_data/b_proportion_90/mix.sam \
+     -t example_data/transcripts.fasta \
+     -P 0.9 \
+     -l 75 \
+     -a type_a_90 \
+     -b type_b_90 \
+     -A 0 \
+     --bias-module 
